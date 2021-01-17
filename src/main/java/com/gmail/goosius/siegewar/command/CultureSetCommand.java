1ca2ce75778acf5bdcd104e9c312de19f17415f6
Goosius1 <aonghus.lynch@gmail.com>
2021-01-17 21:56:01 +0000

    culture bug fixes and improvements

--- src/main/java/com/gmail/goosius/siegewar/command/CultureSetCommand.java
+++ src/main/java/com/gmail/goosius/siegewar/command/CultureSetCommand.java
@@ -19,37 +19,37 @@ import java.util.List;
 public class CultureSetCommand implements CommandExecutor, TabCompleter {
 
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		List<String> allwords = new ArrayList<>();
-		allwords.add("setculture");
-		return allwords;
+		return new ArrayList<>();
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		if(!(sender instanceof Player))
+		if(!(sender instanceof Player) || args == null)
+			return true;
+
+		Player player = (Player)sender;
+		Resident resident = TownyUniverse.getInstance().getResident(player.getName());
+		if (resident == null)
 			return true;
 
 		if (args.length != 1)
 			sender.sendMessage(ChatTools.formatCommand("Eg", "/setculture", "[culture]", ""));
 
-		//Check for permission
-		Player player = (Player)sender;
-		if (!player.hasPermission(SiegeWarPermissionNodes.SIEGEWAR_COMMAND_SIEGEWAR_NATION.getNode(args[0]))) {
+		/*
+		 * Check for resident + mayor
+		 * Note that I didn't add a permission for this
+		 * because it would have required an new manual townyperms setup step,
+		 * thus providing little benefit at the cost of introducing a manual fail point
+		 */
+		if(!resident.hasTown() || !resident.isMayor())
 			player.sendMessage(Translation.of("msg_err_command_disable"));
-			return true;
-		}
-
-		//Check for resident
-		Resident resident = TownyUniverse.getInstance().getResident(player.getName());
-		if (resident == null)
-			return true;
 
 		//Set town culture
 		try {
 			Town town = resident.getTown();
 			TownMetaDataController.setTownCulture(town, args[0]);
+			//Todo - success message
 		} catch (NotRegisteredException e) {
-			//No town //Todo
-			return true;
+			//Won't get here as we already checked for resident
 		}
 
 		return true;
