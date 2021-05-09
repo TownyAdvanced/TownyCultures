2d52740bae9305f0c59b30b60d26ee14511a533b
Llm Dl <LlmDlio@gmail.com>
2021-05-09 06:09:15 -0500

    Adds PreCultureSetEvent, a cancellable event.

---
+++ src/main/java/com/gmail/goosius/townycultures/events/PreCultureSetEvent.java
@@ -0,0 +1,64 @@
+package com.gmail.goosius.townycultures.events;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import com.gmail.goosius.townycultures.settings.Translation;
+import com.palmergames.bukkit.towny.object.Town;
+
+public class PreCultureSetEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+	private final String culture;
+	private final String townName;
+	private final Town town;
+
+	private boolean isCancelled = false;
+	private String cancelMessage = Translation.of("msg_err_command_disable");
+	
+	public PreCultureSetEvent(String culture, Town town) {
+		this.culture = culture;
+		this.town = town;
+		this.townName = town.getName();
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+	
+	public String getCultureName() {
+		return culture;
+	}
+
+	public String getTownName() {
+		return townName;
+	}
+
+	public Town getTown() {
+		return town;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return isCancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		isCancelled = cancelled;
+	}
+
+	public String getCancelMessage() {
+		return cancelMessage;
+	}
+
+	public void setCancelMessage(String cancelMessage) {
+		this.cancelMessage = cancelMessage;
+	}
+}
