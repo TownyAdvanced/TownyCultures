2ba6ed54bee2b4d030b52b7bebdcf9edc27d29b9
Goosius1 <goosius1@gmail.com>
2025-11-06 15:52:32 +0000

    preset cultures - fixed unintended diff change

--- src/main/java/com/gmail/goosius/townycultures/settings/Settings.java
+++ src/main/java/com/gmail/goosius/townycultures/settings/Settings.java
@@ -5,10 +5,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 import com.gmail.goosius.townycultures.objects.PresetCulture;
 import com.gmail.goosius.townycultures.enums.AutomaticCultureSelectionType;
-import com.gmail.goosius.townycultures.utils.PresetCulturesUtil;
 import com.palmergames.bukkit.towny.object.Coord;
 import org.bukkit.plugin.Plugin;
 
@@ -18,151 +18,153 @@ import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.exceptions.initialization.TownyInitException;
 import com.palmergames.bukkit.towny.object.TranslationLoader;
 import com.palmergames.util.FileMgmt;
-import org.jetbrains.annotations.NotNull;
 
 public class Settings {
-    private static CommentedConfiguration config, newConfig;
-    private static Path configPath = TownyCultures.getTownyCultures().getDataFolder().toPath().resolve("config.yml");
-    
-    public static boolean loadSettingsAndLang() {
-        try {
-            loadConfig();
-        } catch (IOException e) {
-            TownyCultures.severe(String.format("Loading error: Failed to load file %s (does it pass a yaml parser?).", configPath));
-            TownyCultures.severe("https://jsonformatter.org/yaml-parser");
-            TownyCultures.severe(e.getMessage());
-            TownyCultures.severe("Config.yml failed to load! Disabling!");
-            return false;
-        }
-        try {
-            Plugin plugin = TownyCultures.getTownyCultures();
-            Path langFolderPath = Paths.get(plugin.getDataFolder().getPath()).resolve("lang");
-            TranslationLoader loader = new TranslationLoader(langFolderPath, plugin, TownyCultures.class);
-            loader.load();
-            TownyAPI.getInstance().addTranslations(plugin, loader.getTranslations());
-        } catch (TownyInitException e) {
-            e.printStackTrace();
-            TownyCultures.severe("Locale files failed to load! Disabling!");
-            return false;
-        }
+	private static CommentedConfiguration config, newConfig;
+	private static Path configPath = TownyCultures.getTownyCultures().getDataFolder().toPath().resolve("config.yml");
 
-        return true;
-    }
+	public static boolean loadSettingsAndLang() {
+		try {
+			loadConfig();
+		} catch (IOException e) {
+			TownyCultures.severe(String.format("Loading error: Failed to load file %s (does it pass a yaml parser?).", configPath));
+			TownyCultures.severe("https://jsonformatter.org/yaml-parser");
+			TownyCultures.severe(e.getMessage());
+			TownyCultures.severe("Config.yml failed to load! Disabling!");
+			return false;
+		}
 
-    private static void loadConfig() throws IOException {
-        if (FileMgmt.checkOrCreateFile(configPath.toString())) {
-            // read the config.yml into memory
-            config = new CommentedConfiguration(configPath);
-            if (!config.load())
-                TownyCultures.severe("Failed to load Config!");
+		try {
+			Plugin plugin = TownyCultures.getTownyCultures(); 
+			Path langFolderPath = Paths.get(plugin.getDataFolder().getPath()).resolve("lang");
+			TranslationLoader loader = new TranslationLoader(langFolderPath, plugin, TownyCultures.class);
+			loader.load();
+			TownyAPI.getInstance().addTranslations(plugin, loader.getTranslations());
+		} catch (TownyInitException e) {
+			e.printStackTrace();
+			TownyCultures.severe("Locale files failed to load! Disabling!");
+			return false;
+		}
 
-            setDefaults(TownyCultures.getTownyCultures().getVersion(), configPath);
-            PresetCulturesUtil.clearPresetCultures();;
-            PresetCulturesUtil.loadPresetCultures();
-            config.save();
-        }
-    }
+		return true;
+	}
+	
+	private static void loadConfig() throws IOException {
+		if (FileMgmt.checkOrCreateFile(configPath.toString())) {
+			// read the config.yml into memory
+			config = new CommentedConfiguration(configPath);
+			if (!config.load())
+				TownyCultures.severe("Failed to load Config!");
 
-    public static void addComment(String root, String... comments) {
+			setDefaults(TownyCultures.getTownyCultures().getVersion(), configPath);
+			config.save();
+		}
+	}
 
-        newConfig.addComment(root.toLowerCase(), comments);
-    }
+	public static void addComment(String root, String... comments) {
 
-    private static void setNewProperty(String root, Object value) {
+		newConfig.addComment(root.toLowerCase(), comments);
+	}
 
-        if (value == null) {
-            value = "";
-        }
-        newConfig.set(root.toLowerCase(), value.toString());
-    }
+	private static void setNewProperty(String root, Object value) {
 
-    private static void setProperty(String root, Object value) {
+		if (value == null) {
+			value = "";
+		}
+		newConfig.set(root.toLowerCase(), value.toString());
+	}
 
-        config.set(root.toLowerCase(), value.toString());
-    }
+	private static void setProperty(String root, Object value) {
 
-    public static String getLastRunVersion(String currentVersion) {
+		config.set(root.toLowerCase(), value.toString());
+	}
 
-        return getString(ConfigNodes.LAST_RUN_VERSION.getRoot(), currentVersion);
-    }
+	public static String getLastRunVersion(String currentVersion) {
 
-    /**
-     * Builds a new config reading old config data.
-     */
-    private static void setDefaults(String version, Path configPath) {
+		return getString(ConfigNodes.LAST_RUN_VERSION.getRoot(), currentVersion);
+	}
 
-        newConfig = new CommentedConfiguration(configPath);
-        newConfig.load();
+	/**
+	 * Builds a new config reading old config data.
+	 */
+	private static void setDefaults(String version, Path configPath) {
 
-        for (ConfigNodes root : ConfigNodes.values()) {
-            if (root.getComments().length > 0)
-                addComment(root.getRoot(), root.getComments());
-            if (root.getRoot() == ConfigNodes.VERSION.getRoot())
-                setNewProperty(root.getRoot(), version);
-            else if (root.getRoot() == ConfigNodes.LAST_RUN_VERSION.getRoot())
-                setNewProperty(root.getRoot(), getLastRunVersion(version));
-            else
-                setNewProperty(root.getRoot(), (config.get(root.getRoot().toLowerCase()) != null) ? config.get(root.getRoot().toLowerCase()) : root.getDefault());
-        }
+		newConfig = new CommentedConfiguration(configPath);
+		newConfig.load();
 
-        config = newConfig;
-        newConfig = null;
-    }
+		for (ConfigNodes root : ConfigNodes.values()) {
+			if (root.getComments().length > 0)
+				addComment(root.getRoot(), root.getComments());
+			if (root.getRoot() == ConfigNodes.VERSION.getRoot())
+				setNewProperty(root.getRoot(), version);
+			else if (root.getRoot() == ConfigNodes.LAST_RUN_VERSION.getRoot())
+				setNewProperty(root.getRoot(), getLastRunVersion(version));
+			else
+				setNewProperty(root.getRoot(), (config.get(root.getRoot().toLowerCase()) != null) ? config.get(root.getRoot().toLowerCase()) : root.getDefault());
+		}
 
-    public static String getString(String root, String def) {
+		config = newConfig;
+		newConfig = null;
+	}
 
-        String data = config.getString(root.toLowerCase(), def);
-        if (data == null) {
-            sendError(root.toLowerCase() + " from config.yml");
-            return "";
-        }
-        return data;
-    }
+	public static String getString(String root, String def) {
 
-    private static void sendError(String msg) {
+		String data = config.getString(root.toLowerCase(), def);
+		if (data == null) {
+			sendError(root.toLowerCase() + " from config.yml");
+			return "";
+		}
+		return data;
+	}
 
-        TownyCultures.severe("Error could not read " + msg);
-    }
+	private static void sendError(String msg) {
 
-    public static boolean getBoolean(ConfigNodes node) {
+		TownyCultures.severe("Error could not read " + msg);
+	}
 
-        return Boolean.parseBoolean(config.getString(node.getRoot().toLowerCase(), node.getDefault()));
-    }
+	public static boolean getBoolean(ConfigNodes node) {
 
-    public static double getDouble(ConfigNodes node) {
+		return Boolean.parseBoolean(config.getString(node.getRoot().toLowerCase(), node.getDefault()));
+	}
 
-        try {
-            return Double.parseDouble(config.getString(node.getRoot().toLowerCase(), node.getDefault()).trim());
-        } catch (NumberFormatException e) {
-            sendError(node.getRoot().toLowerCase() + " from config.yml");
-            return 0.0;
-        }
-    }
+	public static double getDouble(ConfigNodes node) {
 
-    public static int getInt(ConfigNodes node) {
+		try {
+			return Double.parseDouble(config.getString(node.getRoot().toLowerCase(), node.getDefault()).trim());
+		} catch (NumberFormatException e) {
+			sendError(node.getRoot().toLowerCase() + " from config.yml");
+			return 0.0;
+		}
+	}
 
-        try {
-            return Integer.parseInt(config.getString(node.getRoot().toLowerCase(), node.getDefault()).trim());
-        } catch (NumberFormatException e) {
-            sendError(node.getRoot().toLowerCase() + " from config.yml");
-            return 0;
-        }
-    }
+	public static int getInt(ConfigNodes node) {
 
-    public static String getString(ConfigNodes node) {
+		try {
+			return Integer.parseInt(config.getString(node.getRoot().toLowerCase(), node.getDefault()).trim());
+		} catch (NumberFormatException e) {
+			sendError(node.getRoot().toLowerCase() + " from config.yml");
+			return 0;
+		}
+	}
 
-        return config.getString(node.getRoot().toLowerCase(), node.getDefault());
-    }
+	public static String getString(ConfigNodes node) {
 
-    public static void setLastRunVersion(String currentVersion) {
+		return config.getString(node.getRoot().toLowerCase(), node.getDefault());
+	}
 
-        setProperty(ConfigNodes.LAST_RUN_VERSION.getRoot(), currentVersion);
-        config.save();
-    }
+	public static void setLastRunVersion(String currentVersion) {
 
-    public static boolean isTownyCulturesEnabled() {
-        return Settings.getBoolean(ConfigNodes.TOWNY_CULTURES_ENABLED);
-    }
+		setProperty(ConfigNodes.LAST_RUN_VERSION.getRoot(), currentVersion);
+		config.save();
+	}
+
+	public static boolean isTownyCulturesEnabled() {
+		return Settings.getBoolean(ConfigNodes.TOWNY_CULTURES_ENABLED);
+	}
+
+	public static int maxNameLength() {
+		return Settings.getInt(ConfigNodes.MAXIMUM_NAME_LENGTH);
+	}
 
     public static boolean isPresetCulturesEnabled() {
         return getBoolean(ConfigNodes.PRESET_CULTURES_ENABLED);
@@ -192,8 +194,4 @@ public class Settings {
         String cultureDescription = singleCultureAsArray[5].trim();
         return new PresetCulture(topLeftCoord,bottomRightCoord,cultureName,cultureDescription);
     }
-
-    public static int maxNameLength() {
-		return Settings.getInt(ConfigNodes.MAXIMUM_NAME_LENGTH);
-	}
 }
